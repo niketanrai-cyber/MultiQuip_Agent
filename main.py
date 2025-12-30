dbd085a162a2c5f66919e019ca29eb632c6f3e58
niketanrai-cyber <niketan.rai@jadeglobal.com>
2025-12-30 21:45:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -132,10 +132,10 @@ html_content = """
             flex-shrink: 0;
             box-shadow: var(--shadow-sm);
             z-index: 20;
-            height: 80px;
+            height: 110px;
         }
         
-        .logo-img { height: 100px; width: auto; display: block; }
+        .logo-img { height: 90px; width: auto; display: block; }
         .header-title { font-weight: bold; font-size: 24px; color: var(--text-color); margin-left: 15px;}
         .header-controls { display: flex; gap: 10px; }
 
@@ -609,5 +609,6 @@ if os.path.exists("multiquip.png") or os.path.exists("multiquip_title.png"):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
 
 
